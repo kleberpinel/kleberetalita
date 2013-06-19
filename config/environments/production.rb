8176ba62af1dd741144ec9d466d1a8bdcd75ab71
Kleber Pinel <kleberpinel@gmail.com>
2013-06-19 10:51:30 -0300

    contatos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Kleberetalita::Application.configure do
 
-  config.action_mailer.default_url_options = { host: "localhost", port: "3000" }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.raise_delivery_errors = true
